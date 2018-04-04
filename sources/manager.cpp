19e6da905f3c3568daa71187c69b3200bc76a4ae
putanowr <putanowr@L5.pk.edu.pl>
2018-04-04 18:50:00 +0200

    Added handling of --field command line argument.

--- sources/manager.cpp
+++ sources/manager.cpp
@@ -5,8 +5,54 @@
 
 #include <tclap/CmdLine.h>
 #include <easylogging++.h>
+#include <string>
 
-namespace v2g 
+namespace v2g
+{
+
+FieldDef& FieldDef::operator=(const std::string &str)
+{
+  auto delim = std::string("=");
+  auto start = 0U;
+  auto end = str.find(delim);
+  if (end != std::string::npos)
+  {
+    name = str.substr(start, end - start);
+    start = end+1;
+    end = str.size();
+    expression = str.substr(start, end-start);
+  }
+  else
+  {
+    throw TCLAP::ArgParseException(str + " : missing equal sign in file specification");
+  }
+  return *this;
+}
+
+std::ostream& FieldDef::print(std::ostream &out) const
+{
+  out << "\"" << name << "=" << expression << "\"";
+  return out;
+}
+
+std::ostream& operator<<(std::ostream &os, const FieldDef &f)
+{
+    return f.print(os);
+}
+
+} // namespace v2g
+
+namespace TCLAP
+{
+
+template<>
+struct ArgTraits<v2g::FieldDef> {
+    typedef StringLike ValueCategory;
+};
+
+} // namespace TCLAP
+
+namespace v2g
 {
 
 bool Manager::parseCmdArgs(int argc, char *argv[])
@@ -15,6 +61,7 @@ bool Manager::parseCmdArgs(int argc, char *argv[])
   try {
     TCLAP::CmdLine cmd("Convert VTK to GMSH file", ' ', "0.1");
     TCLAP::ValueArg<std::string> outfileArg("o", "outfile", "output file name", false,"v2g.dat","string");
+    TCLAP::ValueArg<FieldDef>     fieldArg("f", "field", "field", false, FieldDef{}, "FieldArg");
     TCLAP::UnlabeledValueArg<std::string> infileArg("infile", "input file name", true, "", "string");
     TCLAP::SwitchArg meshInfoArg("i", "meshinfo", "print mesh info", false);
     TCLAP::SwitchArg noLogArg("s", "nolog", "switch off log messages", false);
@@ -22,11 +69,13 @@ bool Manager::parseCmdArgs(int argc, char *argv[])
     cmd.add(infileArg);
     cmd.add(meshInfoArg);
     cmd.add(noLogArg);
+    cmd.add(fieldArg);
     cmd.parse(argc, argv);
 
     inFile = infileArg.getValue();
     writeInfo = meshInfoArg.getValue();
     noLog = noLogArg.getValue();
+    field = fieldArg.getValue();
 
   } catch(TCLAP::ArgException &e) {
     std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
@@ -37,3 +86,5 @@ bool Manager::parseCmdArgs(int argc, char *argv[])
 }
 
 } // namespace v2g
+
+
--- sources/manager.h
+++ sources/manager.h
@@ -8,11 +8,25 @@
 namespace v2g 
 {
 
+struct FieldDef
+{
+  std::string name = std::string("data");
+  std::string expression = std::string("x");
+  FieldDef(const std::string &fieldName, const std::string &fieldExpression) : name(fieldName), expression(fieldExpression){}
+  FieldDef() = default;
+  // operator= will be used to assign to the FieldDef
+  FieldDef& operator=(const std::string &str);
+  std::ostream& print(std::ostream &out) const;
+};
+
+std::ostream& operator<<(std::ostream &os, const FieldDef &f);
+
 class Manager {
 public:
   bool parseCmdArgs(int argc, char *argv[]);
 
   std::string inFile;
+  FieldDef field;
   bool noLog = false;
   bool writeInfo = false;
   int exitStatus = EXIT_SUCCESS;
