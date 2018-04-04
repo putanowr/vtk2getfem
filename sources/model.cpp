507b19a0f1fcb91516b303ee431bbe3fde6d487e
putanowr <putanowr@L5.pk.edu.pl>
2018-04-04 12:33:43 +0200

    Implementing interpolation of GlobalFunction and access to model components.

--- sources/model.cpp
+++ sources/model.cpp
@@ -1,14 +1,23 @@
 // Copyright (c) 2018 Roman Putanowicz
 //! @brief Simple model of discretised global field.
 
+#include "global_function.h"
 #include "model.h"
+
+#include <getfem/getfem_interpolation.h>
 
 namespace v2g 
 {
 
-getfem::mesh & Model::getMesh()
+Model::Model() :  fem_(mesh_)
 {
-  return mesh_;
+  fem_.set_classical_finite_element(1);
+  model_.add_fem_variable("data", fem_); 
+}
+
+void Model::interpolate(const GlobalFunction &fun)
+{
+  getfem::interpolation_function(fem_, model_.real_variable("data"), fun);
 }
 
 } // namespace v2g
--- sources/model.h
+++ sources/model.h
@@ -10,11 +10,18 @@
 namespace v2g 
 {
 
+class GlobalFunction;
+
 class Model
 {
 public:
-  Model() = default;
-  getfem::mesh &getMesh();
+  using Vector = getfem::model_real_plain_vector;
+  Model();
+  getfem::mesh &getMesh() { return mesh_; }
+  const getfem::mesh &getMesh() const { return mesh_;}
+  const getfem::mesh_fem &getFem(const std::string &name) const { return model_.mesh_fem_of_variable(name); }
+  const Vector & getDOFs(const std::string &name) const { return model_.real_variable(name); }
+  void interpolate(const GlobalFunction &fun);
   
 private:
   getfem::mesh mesh_;
